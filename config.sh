671caacbbe9b62b86b7791a9cf45a18a04f5d637
Zhang Fuxin <zhangfx@lemote.com>
2013-06-20 19:19:31 +0800

    add config.sh for configure in x86_64 linux save.c: add -m32 to trace object build command since we support only ia32 testsuite: add -m32 and -Wl,--hash-style=sysv to build 32 bit objects and            using old hash(.gnu.hash is not yet supported:

---
+++ config.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./configure --enable-32 --enable-readline --libasm-ia32 --libasm-arm --libasm-packed --enable-testing --use-ctags --set-lib-path "/lib;/lib/i386-linux-gnu;/usr/lib/i386-linux-gnu;/usr/lib;/usr/local/lib"
